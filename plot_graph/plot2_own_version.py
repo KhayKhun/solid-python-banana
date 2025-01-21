7defd45aa085e307b885b8e0bdeff31f29a85066
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2025-01-21 15:26:25 +0700

    Finalize plot 2

--- plot_graph/plot2_own_version.py
+++ plot_graph/plot2_own_version.py
@@ -12,14 +12,19 @@ def f(x):
     return x**2
 
 pygame.init()
-
 w = 600
 h = 600
+
+sample_count = 60
 
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("Draw graph")
 
 running = True
+
+start_x = -40
+end_x = 30
+y_scale = 600 / 900
 
 def match_coordinate_to_screen(x):
     return x * 10
@@ -32,24 +37,28 @@ while running:
 
     screen.fill((0, 0, 0))
 
+    current = start_x
+
     prev_x = None
     prev_y = None
 
-    for x in range(w):
+    while current <= end_x:
+        x = current
+        y = f(x) * y_scale
 
-        actual_x = x * (1/150) - 2
-        actual_y = f(actual_x)
 
-        screen_y = h - int(actual_y * 150) - 1
+        screen_x = match_coordinate_to_screen(x + 30)
+        screen_y = int(h - y) - 1
+        # print(screen_x, screen_y)
+        
+        if prev_x and prev_y:
+            pygame.draw.line(screen, (255, 255, 255), (screen_x, screen_y), (prev_x, prev_y))
 
-        screen.set_at((x, screen_y), (255, 255, 255))
-
-        if prev_x:
-            pygame.draw.line(screen, (255, 255, 255), (x, screen_y), (prev_x, prev_y))
-
-        prev_x = x
+        screen.set_at((screen_x, screen_y), (255, 255, 255))
+        current += 1
+        
+        prev_x = screen_x
         prev_y = screen_y
-
 
     pygame.display.flip()
 
