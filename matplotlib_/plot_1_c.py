722b827f7aae857ba5eb3adcb58979a98ee126b6
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2025-01-23 09:55:39 +0700

    Plot 1 c

---
+++ matplotlib_/plot_1_c.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return np.arctan(x)
+
+x = np.linspace(-10, 10, 400)
+y = f(x)
+
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, label=r'$f(x) = \arctan x$', color='blue')  # \cdot -> . instead of *
+
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(r'$Graph \ of \ f(x) = \arctan x$')
+
+plt.grid(True)
+plt.legend() # add some extra legend ui 
+
+plt.show()
