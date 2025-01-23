c005ce63ac19cf13625466d4a2396f89109d20bf
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2025-01-23 10:43:54 +0700

    Plot point and distances

--- matplotlib_/read_csv.py
+++ matplotlib_/read_csv.py
@@ -1,7 +1,20 @@
 import numpy as np
- 
-# using loadtxt()
+import matplotlib.pyplot as plt
+
 distances = np.loadtxt("distances.csv", delimiter=',')
 points = np.loadtxt("points.csv", delimiter=',')
 
-print(distances,type(distances[0]))
+# print(len(distances), distances) # 1D ndarray
+# print(len(points), points) # 2D ndarray [x,y]
+
+plt.figure(figsize=(8, 6)) # color in range -> c = distances
+sc = plt.scatter(x=points[:, 0], y=points[:, 1], c=distances, cmap="cool")
+
+plt.colorbar(sc, label="Distance Value")
+
+plt.xlabel("X Coordinate")
+plt.ylabel("Y Coordinate")
+plt.title("Points and Distances")
+
+plt.grid(True)
+plt.show()
