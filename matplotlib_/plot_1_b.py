15bddf278aab6ee8f0391acc39fc1f7c44063fde
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2025-01-23 09:50:17 +0700

    Plot 1 n

---
+++ matplotlib_/plot_1_b.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x * np.sin(2*x)
+
+x = np.linspace(-10, 10, 400)
+y = f(x)
+
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, label=r'$f(x) = x \cdot \sin(2x)$', color='blue')  # \cdot -> . instead of *
+
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(r'$Graph \ of \ f(x) = x \cdot \sin(2x)$')
+
+plt.grid(True)
+plt.legend()
+
+plt.show()
